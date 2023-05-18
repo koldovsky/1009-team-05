a64a01b6d2e59d7af83bc24fdfbcaca631b753fd
taras55555 <94987003+taras55555@users.noreply.github.com>
2023-05-18 16:31:05 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+//code
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+//code
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+//code
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+//code
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+//code
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+//code
