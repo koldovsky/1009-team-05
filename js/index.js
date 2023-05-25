5332dfac5dd4572738322bbe5731bb31c539f76a
taras55555 <tkaminskyi@gmail.com>
2023-05-25 22:40:33 +0300

    add animation for hamburger button

--- js/index.js
+++ js/index.js
@@ -1,12 +1,38 @@
 function show_hide_hamburger() {
-    var display_value = document.getElementById("hamburger-list").style.display
-    var body_overflow = document.getElementsByTagName("body");
-    if (display_value == "unset") {
-        document.getElementById("hamburger-list").style.display = "none";
-        body_overflow[0].style.overflow = "unset";
+    const calculateScrollBarWidth = () => window.innerWidth - document.documentElement.clientWidth;
+    const bodySelector = document.querySelector("body");
+    const hamburgerButton = document.querySelector(".header__hamburger-button");
+    const groupLines = document.querySelector(".header__hamburger-group-lines");
+    const crossSign = document.querySelector(".header__x-ham");
+    const hamburgerList = document.querySelector("#hamburger-list");
+
+    if (hamburgerList.style.display === "unset") {
+        hamburgerButton.style.marginRight = "0px";
+        hamburgerList.style.display = "none";
+        bodySelector.style.overflow = "unset";
+
+        setTimeout(function () {
+            groupLines.style.width = "36px";
+            groupLines.style.height = "32px";
+            groupLines.style.transition = "width 150ms, height 550ms";
+        }, 500);
+
+        crossSign.style.width = "0px";
+        crossSign.style.height = "0px";
+        crossSign.style.transition = "width 500ms, height 500ms";
     }
     else {
-        document.getElementById("hamburger-list").style.display = "unset";
-        body_overflow[0].style.overflow = "hidden";
+        hamburgerButton.style.marginRight = calculateScrollBarWidth() + "px";
+        hamburgerList.style.display = "unset";
+        bodySelector.style.overflow = "hidden";
+        groupLines.style.height = "3px";
+        groupLines.style.width = "0px";
+        groupLines.style.transition = "height 150ms, width 550ms";
+        setTimeout(function () {
+            crossSign.style.width = "36px";
+            crossSign.style.height = "32px";
+            crossSign.style.transition = "width 500ms, height 500ms";
+        }, 550);
     }
 }
+
