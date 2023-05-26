e2c2dec05c3c399783c0d589529b6edefa0dc87a
taras55555 <tkaminskyi@gmail.com>
2023-05-26 16:56:40 +0300

    added deadline countdown

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+// hamburger menu
 let timeoutId;
 let blockedRunning = false;
+
 function show_hide_hamburger() {
     timeoutId = setTimeout(function () {
         blockedRunning = false;
@@ -45,3 +47,37 @@ function show_hide_hamburger() {
         }
     }
 }
+//end hamburger menu
+
+
+// countdown
+setInterval(function () {
+    let currentTime = new Date();
+    const deadLine = new Date("2023-06-06 10:10:10");
+    let difference = deadLine - currentTime;
+    let timeFragment = convertMilliseconds(difference);
+    document.querySelector(".header__countdown-days").innerHTML = timeFragment.days;
+    document.querySelector(".header__countdown-housr").innerHTML = timeFragment.hours + ":";
+    document.querySelector(".header__countdown-minutes").innerHTML = timeFragment.minutes + ":";
+    document.querySelector(".header__countdown-seconds").innerHTML = timeFragment.seconds;
+
+}, 1000);
+
+function convertMilliseconds(milliseconds) {
+    const SECOND = 1000;
+    const MINUTE = SECOND * 60;
+    const HOUR = MINUTE * 60;
+    const DAY = HOUR * 24;
+    const days = Math.floor(milliseconds / DAY);
+    const hours = Math.floor((milliseconds % DAY) / HOUR).toString().padStart(2, '0');
+    const minutes = Math.floor((milliseconds % HOUR) / MINUTE).toString().padStart(2, '0');
+    const seconds = Math.floor((milliseconds % MINUTE) / SECOND).toString().padStart(2, '0');
+
+    return {
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
+  // end countdown
