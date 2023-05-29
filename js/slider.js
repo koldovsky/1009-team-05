2612855794f553a5c1a9fce0aaed629ea9de1c2c
taras55555 <tkaminskyi@gmail.com>
2023-05-29 12:47:28 +0300

    disabled double/triple click on slider

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,21 @@
-const btnLeft = document.querySelector("#leftBtn");
-const btnRight = document.querySelector("#rightBtn");
+const btnLeft = document.querySelector("#scrollLeft");
+const btnRight = document.querySelector("#scrollRight");
 const mainBlockContainer = document.querySelector(".main__booklets-menu-container");
 const mainBlockMenu = document.querySelector(".main__booklets-menu-block");
+const fullBookletWidth = 330;
+let isScrolling = false;
+
+function handleButtonClick() {
+    if (isScrolling) {
+        return;
+    }
+    isScrolling = true;
+    this.id === "scrollLeft" ? mainBlockContainer.scroll({ left: mainBlockContainer.scrollLeft - fullBookletWidth, behavior: "smooth", }) : mainBlockContainer.scroll({ left: fullBookletWidth + mainBlockContainer.scrollLeft, behavior: "smooth", });
+    setTimeout(function () {
+        isScrolling = false;
+    }, 300);
+}
 
 btnLeft.addEventListener('click', handleButtonClick);
 btnRight.addEventListener('click', handleButtonClick);
 
-function handleButtonClick() {
-    this.id === "leftBtn" ? mainBlockContainer.scroll({ left: mainBlockContainer.scrollLeft - 330, behavior: "smooth", }) : mainBlockContainer.scroll({ left: 330 + mainBlockContainer.scrollLeft, behavior: "smooth", });
-}
