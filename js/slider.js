13a84a96640d456d78924832d3cfb892b7206123
taras55555 <tkaminskyi@gmail.com>
2023-05-30 12:45:19 +0300

    added closed loop on slider

--- js/slider.js
+++ js/slider.js
@@ -4,18 +4,44 @@ const mainBlockContainer = document.querySelector(".main__booklets-menu-containe
 const mainBlockMenu = document.querySelector(".main__booklets-menu-block");
 const fullBookletWidth = 330;
 let isScrolling = false;
+let autoInterval;
+let defaultDirectionScroll = "scrollRight";
+runScroll();
 
-function handleButtonClick() {
+function handleButtonClick(currentId) {
+    runScroll();
     if (isScrolling) {
         return;
     }
     isScrolling = true;
-    this.id === "scrollLeft" ? mainBlockContainer.scroll({ left: mainBlockContainer.scrollLeft - fullBookletWidth, behavior: "smooth", }) : mainBlockContainer.scroll({ left: fullBookletWidth + mainBlockContainer.scrollLeft, behavior: "smooth", });
+    currentId === "scrollLeft" ? (mainBlockContainer.scroll({ left: mainBlockContainer.scrollLeft - fullBookletWidth, behavior: "smooth", }), defaultDirectionScroll = "scrollLeft") : (mainBlockContainer.scroll({ left: fullBookletWidth + mainBlockContainer.scrollLeft, behavior: "smooth", }), defaultDirectionScroll = "scrollRight");
     setTimeout(function () {
         isScrolling = false;
-    }, 300);
+    }, 500);
 }
 
-btnLeft.addEventListener('click', handleButtonClick);
-btnRight.addEventListener('click', handleButtonClick);
+btnLeft.addEventListener('click', function () { handleButtonClick(this.id); });
+btnRight.addEventListener('click', function () { handleButtonClick(this.id); });
 
+function runScroll() {
+    clearInterval(autoInterval);
+    autoInterval = setInterval(function () {
+        if (isScrollEnd()) {
+            defaultDirectionScroll = "scrollLeft";
+        }
+        if (isScrollStart()) {
+            defaultDirectionScroll = "scrollRight";
+        }
+        handleButtonClick(defaultDirectionScroll);
+    }, 3000);
+}
+
+function isScrollEnd() {
+    let isEnd = mainBlockContainer.scrollLeft + mainBlockContainer.clientWidth >= mainBlockContainer.scrollWidth;
+    return isEnd;
+}
+
+function isScrollStart() {
+    let isStart = mainBlockContainer.scrollLeft + mainBlockContainer.clientWidth === mainBlockContainer.clientWidth;
+    return isStart;
+}
