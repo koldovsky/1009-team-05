a422d3be3641a378d8e097af99e83da1a9cecf2b
3rgrt <124722737+3rgrt@users.noreply.github.com>
2023-06-04 11:25:15 +0000

    menu js changed

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,4 @@
-const setPic = document.querySelector(".menu-img");
-const btnMenu = document.querySelector(".menu-btn");
-
-const picsArray = ["images/menu/menu-sale/sweetj.png", "images/menu/menu-sale/gentlec.png", "images/menu/menu-sale/rosesp.png"];
-
-function choosingPic() {
-    const randomIndex = Math.floor(Math.random() * picsArray.length);
-    setPic.src = picsArray[randomIndex];
-}
-
-btnMenu.addEventListener("click", choosingPic);
+document.addEventListener('partialsLoaded', () => {
+    import('./hamburger-menu.js');
+    import('./menu-main.js');
+});
