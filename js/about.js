2ddd0b0de3d5410b5795073dee6225f8db78eb7b
taras55555 <tkaminskyi@gmail.com>
2023-06-06 13:50:56 +0300

    Import is-ready-state.js

--- js/about.js
+++ js/about.js
@@ -1,3 +1,4 @@
 document.addEventListener('partialsLoaded', () => {
     import('./hamburger-menu.js');
+    import('./is-ready-state.js');
 });
--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 document.addEventListener('partialsLoaded', () => {
     import('./hamburger-menu.js');
     import('./menu-main.js');
+    import('./is-ready-state.js');
 });
